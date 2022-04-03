1d186c7d93af1663284161d3cebc886375768f7a
Brad Weiner <brad.weiner@colorado.edu>
2022-04-03 14:49:56 -0600

    adding single source file

---
+++ code/r/3_clean.R
@@ -0,0 +1,84 @@
+##RUN AND DO STUFF HERE
+
+#clear memory
+rm(list=ls())
+
+setwd("~/Dropbox/Side Projects/fly_the_w_bot")
+
+source("code/r/0_creds.R")
+source("code/r/1_load.R")
+
+shut_down_signal <- FALSE
+
+games <- get_mlb_data()
+
+get_cubs_game_id()
+
+get_live_feed_data()
+
+create_live_feed_df()
+
+head(cubs)
+
+# Shut down server if Cubs aren't playing today
+
+if(nrow(cubs) == 0){
+        
+        stop_ec2_instance()
+        
+}
+
+# Post Tweet if Cubs Won
+
+if (cubs$cubs_w_or_l == "Cubs Won" &
+   cubs$game_status %in% complete_game_statuses &
+   cubs$id %ni% already_tweeted$id &
+   nchar(cubs$winpitch) > 0){
+        
+        create_w_tweet()
+        
+        post_tweet(
+                status = w_tweet,
+                token = token,media = "cubs_w_flag.png")
+        
+        already_tweeted <- rbind(already_tweeted,cubs)
+        
+        saveRDS(already_tweeted,"data/transformed/already_tweeted_2022.rds")
+        
+        shut_down_signal <- TRUE
+        }
+
+
+# Shut Down Server if Cubs Lost
+
+###ADD A SYSTEM SHUT DOWN SIGNAL IF THE GAME IS OVER AND THE CUBS LOST
+
+if(cubs$cubs_w_or_l == "Cubs Lost" &
+   cubs$game_status %in% complete_game_statuses){
+        
+        shut_down_signal <- TRUE
+}
+
+# Add Row to croncheck log
+
+add_to_croncheck()
+
+# Remove Rtweet tokens
+
+# system(command = "rm .rtweet_token*")
+
+# Shut down server if shut down signal is TRUE
+
+if(shut_down_signal == TRUE){
+        
+        ## put code here to push data files to git
+        
+        ## wait ten seconds
+        
+        # stop ec2 instnace
+        stop_ec2_instance()
+        
+        
+}
+
+rm(list=ls())
